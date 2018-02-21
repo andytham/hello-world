7902d8c73ad6075c9c133b5e5a6244942770b9d9
andy tham <thamandyy@gmail.com>
2018-02-21 10:16:05 -0500

    switched to older version of d3 dependencies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
-import d3 from 'd3';
+import * as d3 from 'd3';
 import Datamap from 'datamaps';
+// import map from './datamaps.world.min.js'
+import './map.css'
+
+var mapStyle = {
+  position: 'relative',
+  width: '500px',
+  height: '300px'
+}
+
 
 export default class helloWorld extends React.Component {
   constructor(props){
@@ -19,21 +28,45 @@ export default class helloWorld extends React.Component {
   }
 
   renderMap(){
-    var map = new Datamap({element: document.getElementById('container')});
-    this.setState({
-      isRendered: true
-    })
+    var basic_choropleth = new Datamap({
+      element: document.getElementById("container"),
+      projection: 'mercator',
+      fills: {
+        defaultFill: "#ABDDA4",
+        authorHasTraveledTo: "red"
+      },
+      data: {
+        USA: { fillKey: "authorHasTraveledTo" },
+        JPN: { fillKey: "authorHasTraveledTo" },
+        ITA: { fillKey: "authorHasTraveledTo" },
+        CRI: { fillKey: "authorHasTraveledTo" },
+        KOR: { fillKey: "authorHasTraveledTo" },
+        DEU: { fillKey: "authorHasTraveledTo" },
+      }
+    });
+
+    var colors = d3.scale.category10();
+    window.setInterval(function() {
+      console.log("colors?");
+      basic_choropleth.updateChoropleth({
+        USA: colors(Math.random() * 10),
+        RUS: colors(Math.random() * 100),
+        AUS: { fillKey: 'authorHasTraveledTo' },
+        BRA: colors(Math.random() * 50),
+        CAN: colors(Math.random() * 50),
+        ZAF: colors(Math.random() * 50),
+        IND: colors(Math.random() * 50),
+      });}, 2000
+    );
   }
 
   render(){
     return(
       <div>
-        <div>
+        <div className="">
           hello world
         </div>
-
-
-        <div id="container" style="position: relative; width: 500px; height: 300px;"></div>
+        <div id="container" style={mapStyle}></div>
         <div>{this.renderMap()} </div>
       </div>
     )
