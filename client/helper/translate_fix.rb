3b74b60976d625d49038d5d4f69d1ccbcd3c9644
andy tham <thamandyy@gmail.com>
2018-02-22 15:55:39 -0500

    translation json fixed, seeds seeded

---
+++ client/helper/translate_fix.rb
@@ -0,0 +1,37 @@
+project_id    = "sublime-lambda-196016"
+# text          = "The text you would like to translate"
+# language_code = "The ISO 639-1 code of language to translate to, eg. 'en'"
+ENV["GOOGLE_APPLICATION_CREDENTIALS"] = "client/helper/My First Project-5a2ab66edddd.json"
+require "google/cloud/translate"
+require "json"
+require_relative "create_list_639_codes.rb"
+
+
+text = "hello"
+
+
+dictionary = []
+tempHash = {}
+@language_array.each do |lang|
+  language_code = lang
+  hash = Hash.new
+  hash["language"] = lang
+  begin #google will throw an error if they do not have the language specified
+    translate   = Google::Cloud::Translate.new project: project_id
+    translation = translate.translate text, from: 'en' , to: language_code
+    puts "Translated '#{text}' to '#{translation.text.inspect}'"
+    puts "Original language: #{translation.from} translated to: #{translation.to}"
+    if !hash[text]
+      hash[text] = translation.text.inspect.to_str
+    end
+  rescue
+    puts "Most likely language not found"
+  end
+
+  dictionary << hash
+end
+puts dictionary
+puts "dictionary outputted"
+
+result = File.new("client/helper/translated_fix.json", mode="w+")
+result.write(JSON.generate(dictionary))
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json';
+countrylistFile = File.read "client/helper/combined_countries.json"
+countrylist = JSON.parse(countrylistFile)
+# Country.create!()
+countrylist.each do |country, info|
+  Country.create!(country: country, name: info["name"], native: info["native"], phone: info["phone"], continent: info["continent"], capital: info["capital"], currency: info["currency"], languages: info["languages"])
+end
+
+puts "#{Country.count} countries created"
+
+translatedFile = File.read "client/helper/translated_fix.json"
+translated = JSON.parse(translatedFile)
+
+translated.each do |lang|
+  if lang["hello"]
+    Translation.create!(language: lang["language"], hello: lang["hello"])
+  end
+end
+
+puts "#{Translation.count} translations created"
